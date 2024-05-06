d5794e65f656d19f4822f7b2fe8bb03c33bb9702
Daniel160407 <abulashvilidani@hotmail.com>
2024-05-06 19:47:42 +0400

    Added ability to move paddles

--- Application.cpp
+++ Application.cpp
@@ -5,15 +5,48 @@ int main(){
     window.setFramerateLimit(60);
 
     sf::RectangleShape leftPaddle(sf::Vector2f(20, 100));
-    leftPaddle.setPosition(100,100);
+    leftPaddle.setPosition(50, 250);
     leftPaddle.setFillColor(sf::Color::White);
     
     sf::RectangleShape rightPaddle(sf::Vector2f(20, 100));
     rightPaddle.setPosition(730, 250);
     rightPaddle.setFillColor(sf::Color::White);
 
+    sf::CircleShape ball(10);
+    ball.setPosition(390, 290);
+    ball.setFillColor(sf::Color::White);
+
+    float ballSpeedX = -4;
+    float ballSpeedY = -4;
+
     while(window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && leftPaddle.getPosition().y > 0){
+            leftPaddle.move(0, -5);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && leftPaddle.getPosition().y + leftPaddle.getSize().y < window.getSize().y){
+            leftPaddle.move(0, 5);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rightPaddle.getPosition().y > 0){
+            rightPaddle.move(0, -5);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rightPaddle.getPosition().y + rightPaddle.getSize().y < window.getSize().y){
+            rightPaddle.move(0, 5);
+        }
+
+        window.clear();
+
         window.draw(leftPaddle);
         window.draw(rightPaddle);
+        window.draw(ball);
+
+        window.display();
     }
 }
